ce8b2b522a84f677e2a4eeef7cb3aa86929a15f5
Skymax <crespo.maximo@gmail.com>
2025-02-26 19:47:10 -0300

    update: counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
+import { onMount } from 'svelte'
 import NumberFlow from '@number-flow/svelte'
+
+let counter = $state(0)
+
+onMount(() => {
+	const interval = setInterval(() => {
+		counter += 1
+	}, 1000)
+
+	return () => clearInterval(interval)
+})
 </script>
 
-<NumberFlow value={123} />
+<NumberFlow
+	value={counter}
+	prefix="$"
+  format={{
+    style: 'decimal',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }}
+/>
